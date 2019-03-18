dc4ddd694f2f1ec3bc39855a65dd9d34afd2a7e7
Jacky <351181429@qq.com>
2019-03-18 20:13:33 +0800

    messenger返回数据给客户端

--- app/src/main/java/com/hechuangwu/server/service/MessengerServer.java
+++ app/src/main/java/com/hechuangwu/server/service/MessengerServer.java
@@ -7,6 +7,7 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.os.RemoteException;
 import android.util.Log;
 
 /**
@@ -14,18 +15,31 @@ import android.util.Log;
  * 功能:
  */
 public class MessengerServer extends Service {
-    private static final int MSG = 1001;
-    private static final String MSG_DATA="msg_data";
+    private static final int MSG_RECEIVER = 1001;
+    private static final int MSG_REPLY= 1002;
+    private static final String MSG_RECEIVER_DATA="msg_receiver_data";
+    private static final String MSG_REPLY_DATA="msg_reply_data";
     private static Messenger mMessenger = new Messenger( new MessengerHandler() );
     private static class MessengerHandler extends Handler{
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage( msg );
             switch (msg.what){
-                case MSG:
+                case MSG_RECEIVER:
                     Bundle data = msg.getData();
-                    String msgData = data.getString( MSG_DATA );
+                    String msgData = data.getString( MSG_RECEIVER_DATA );
                     Log.i( "data", "handleMessage: >>>"+msgData );
+                    Messenger replyTo = msg.replyTo;
+                    Message message = Message.obtain();
+                    message.what = MSG_REPLY;
+                    Bundle bundle = new Bundle();
+                    bundle.putString( MSG_REPLY_DATA,"hello client ,i receiver" );
+                    message.setData( bundle );
+                    try {
+                        replyTo.send( message );
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
                     break;
             }
         }
